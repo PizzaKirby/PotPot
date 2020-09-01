1916ab8fd68b7292e45d70f6b6932a46a57a61a4
PizzaKirby <pizzakirby@gmail.com>
2020-09-01 14:53:05 +0200

    added id interface

--- Calamity/CalamityID.cs
+++ Calamity/CalamityID.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria.ModLoader;
 
 namespace PotPot.Calamity
 {
@@ -17,14 +18,28 @@ namespace PotPot.Calamity
         {
         }
 
-        public void InitItems()
+        public void AddItem(ModItem item)
         {
-
+            Item.Add(item.Name, PotPot.Instance.Calamity.ItemType(item.Name));
         }
 
-        public void InitBuffs()
+        public void AddBuff(ModBuff buff)
         {
+            Buff.Add(buff.Name, PotPot.Instance.Calamity.BuffType(buff.Name));
+        }
 
+        public int GetBID(string buffname)
+        {
+            if (Buff.TryGetValue(buffname, out int retval))
+                return retval;
+            return -1;
+        }
+
+        public int GetIID(string itemname)
+        {
+            if(Item.TryGetValue(itemname, out int retval))
+                return retval;
+            return -1;
         }
 
         public static CalamityID Instance
